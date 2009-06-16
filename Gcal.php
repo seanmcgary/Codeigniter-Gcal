6c3ba5330740f6d13c5ec0825ab95df0cfaba988
Sean McGary <sean.mcgary@gmail.com>
2009-06-15 21:54:39 -0400

    Added comments to code. Comments are Javadoc styled for easy reading

--- Gcal.php
+++ Gcal.php
@@ -15,7 +15,7 @@
 // fire up the Zend Loader
 // we are going to assume that you have a path in
 // your php.ini set to look for the ZendGdata library
-// if you dont, then do the following:
+// if you dont, then do the following instead of the current:
 //
 // require_once 'path/to/ZendGdata/library';
 require_once 'Zend/Loader.php';
@@ -24,12 +24,18 @@ Zend_Loader::loadClass('Zend_Gdata_AuthSub');
 Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
 Zend_Loader::loadClass('Zend_Gdata_Calendar');
  
-class Gcal {
+class Gcal {   
 
-    
-    
-    
-	
+    /**
+     * Logs in via AuthSub. AuthSub is simiar to OAuth, but was developed
+     * before OAuth became the "standard". AuthSub acts in a similar way as
+     * OAuth. You login through Google, granting access to the site. You are
+     * then returned to the site with an access token to authorize with.
+     *
+     * @param String $redirectUrl - URL that you want to redirect to
+     *                              once a token is assigned
+     * @return String - URL to follow to log into Google Calendar
+     */
 	function getAuthSubUrl($redirectUrl){	
 		// indicates the app will only access Google Calendar feeds
 		$scope = 'http://www.google.com/calendar/feeds';
@@ -38,28 +44,60 @@ class Gcal {
 		return Zend_Gdata_AuthSub::getAuthSubTokenUri($redirectUrl, $scope, $secure, $session);	
 	}
 	
-	
+	/**
+     * ClientLogin is simplier in that you dont need to be redirected
+     * to sign in. Instead, simply provide your Google account credentials
+     * and it works its magic, returning an access token. Generally ClientLogin
+     * is reserved for  installed applications, but I provided access to it
+     * anyway incase someone wants to access a Google Calendar without redirecting.
+     *
+     * @param String $username - Google account username
+     * @param String $password = Google account password
+     * @return String - token used for verification.
+     */
 	function clientLogin($username, $password){
 		$service = Zend_Gdata_Calendar::AUTH_SERVICE_NAME; 
 		$client = Zend_Gdata_ClientLogin::getHttpClient($username,$password,$service);
 		
 		return $client;
 	}
-	
+
+    /**
+     * Grabs a list of a users calendars
+     *
+     * @param String $client - Access token
+     * @return calendars in array form
+     */
 	function outputCalendarList($client){
 		$gdataCal = new Zend_Gdata_Calendar($client);
 		$calFeed = $gdataCal->getCalendarListFeed();
 		
 		return $calFeed;
 	}
-	
+
+    /**
+     * List all events in user's calendar
+     * 
+     * @param String $client - Access token
+     * @return events in array form
+     */
 	function outputCalendarEvents($client){
 		$gdataCal = new Zend_Gdata_Calendar($client);
 		$eventFeed = $gdataCal->getCalendarEventFeed();
 		
 		return $eventFeed;
 	}
-	
+
+    /**
+     * List events witin a specified range
+     *
+     * @param String $client - Access token
+     * @param String $startDate - date 'YYYY-MM-DD'
+     *               RFC 3339 timestamp format compatible
+     * @param String $endDate - date 'YYYY-MM-DD'
+     *               RFC 3339 timestamp format compatible
+     * @return events in array form
+     */
 	function calEventsByDateRange($client, $startDate, $endDate){
 		$gdataCal = new Zend_Gdata_Calendar($client);
 		$query = $gdataCal->newEventQuery();
@@ -74,7 +112,14 @@ class Gcal {
 		
 		return $eventFeed;
 	}
-	
+	/**
+     * List events matching a full text query. Full text query searches
+     * both title and event description for matches
+     *
+     * @param String $client - Access token
+     * @param String $eventQuery - Query in string form
+     * @return list of matching events in array form
+     */
 	function calFullTextQuery($client, $eventQuery){
 		$gdataCal = new Zend_Gdata_Calendar($client);
 		$query = $gdataCal->newEventQuery();
@@ -85,7 +130,20 @@ class Gcal {
 		$eventFeed = $gdataCal->getCalendarEventFeed($query);
 		return $eventFeed;
 	}
-	
+
+    /**
+     * Create an event
+     * @param String $client Access token
+     * @param Associative Array $eventArray
+     *                          'desc'      =>  'This is a description for the event',
+								'where'     =>  'Location',
+								'startDate' =>  'YYYY-MM-DD',
+								'startTime' =>  'HH:MM', // 24hr time
+								'endDate'   =>  'YYYY-MM-DD',
+								'endTime'   =>  'HH:MM', // 24hr time
+								'tzOffset'  =>  '00' // timezone offset from GMT
+     * @return String - ID of created event
+     */
 	function createEvent($client, $eventArray){
 		$gdataCal = new Zend_Gdata_Calendar($client);
 		$newEvent = $gdataCal->newEventEntry();
@@ -105,19 +163,29 @@ class Gcal {
 		return $createdEvent->id->text;
 		
 	}
-	
+
+    /**
+     * Create a quick event from string input.
+     * 
+     * Should include time (am/pm), day of the week.
+     *
+     * Day of the week can be specified as "tomorrow"
+     *
+     * Can specify week as well with "next tuesday"
+     *
+     * A day without a modifier will default to that current week,
+     * even if the day has already past
+     *
+     * @param String $client - Access token
+     * @param String $quickAddText - Event description
+     */
 	function createQuickEvent($client, $quickAddText){
 		$gdataCal = new Zend_Gdata_Calendar($client);
 		$event = $gdataCal->newEventEntry();
 		$event->content = $gdataCal->newContent($quickAddText);
 		$event->quickAdd = $gdataCal->newQuickAdd('true');
 		$newEvent = $gdataCal->insertEvent($event);
-	}
-	
-	
-	
-	
-	
+	}	
 }
 
 ?>
